5ffb58ae3dd5f7ac32af133d7ca2e8199d7e5ab1
Ata Kuyumcu <ata.kuyumcu@protonmail.com>
2018-01-30 14:33:13 +0300

    fixed plugin metadata

--- init.rb
+++ init.rb
@@ -6,11 +6,11 @@ require 'pdf_patch'
 
 Redmine::Plugin.register :redmine_hide_estimated_hours do
   name 'Hide estimated hours'
-  author 'Dominique Lederer (return1)'
+  author 'Ata Kuyumcu (LVMBDV)'
   description 'This Redmine plugin reuses the "view_time_entries" permission to hide the estimated hours field'
   version '1.0.9'
-  url 'https://github.com/return1/redmine_hide_estimated_hours'
-  author_url 'http://return1.at/'
+  url 'https://github.com/lvmbdv/redmine_hide_estimated_hours'
+  author_url 'https://lvmbdv.github.io/'
 
   requires_redmine :version_or_higher => '3.4.0'
 
